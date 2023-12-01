170936fc44a0e94e53a5e706e3dccfb013deed4c
杨伟杰 <sia-fl@outlook.com>
2023-12-01 08:15:03 +0800

    更新: 避免让用户看到不必要的报错堆栈信息

--- src/prenv.ts
+++ src/prenv.ts
@@ -7,8 +7,13 @@ import path from 'node:path';
 const { name, dir, commands } = getArgs();
 const parsed = getNamedEnvValues(name, path.join(process.cwd(), dir));
 
-execSync(commands, {
-  stdio: 'inherit',
-  env: parsed,
-  cwd: process.cwd()
-});
+try {
+  execSync(commands, {
+    stdio: 'inherit',
+    env: parsed,
+    cwd: process.cwd()
+  });
+} catch (_) {
+  // 避免输出自身报错堆栈，因为这样会让用户误以为是 prenv 报错
+  process.exit(1);
+}
